ba9ca0442aba25edb214732625b87209dc42f094
blablo <blablo@gmail.com>
2013-01-31 19:12:00 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corobook::Application.config.secret_token = '8c995f978c79f2e320d989f5f991375ef281ba6e16c9b2b31493a591c13fd08b02916ee99da95dbbc975a22b219a44c20b15735788d613b1fd29b255a38f1240'
